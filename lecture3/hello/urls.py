a8eda76626a4e1a9e995854570d2839af5f4f050
Medkelawit_Abadina <mekdelawitkassa6@gmail.com>
2025-06-13 13:48:02 +0300

    new:

--- lecture3/hello/urls.py
+++ lecture3/hello/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns = [
     path("",views.index,name ="index"),
     path("mekdi",views.mekdi ,name ="mekdi"),
-    path("happy",views.happy,name="happy")
+    path("happy",views.happy,name="happy"),
+    path("<str:name>",views.greet,name="greet")
 ]
--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("hello,mekdi")
+    return render(request,"hello/index.html")
 def mekdi(request):
     return HttpResponse("I love mekdi")
 def happy(request):
     return HttpResponse("I love my happy")
+def greet(request,name):
+    return render(request,"hello/greet.html",{"name":name})
